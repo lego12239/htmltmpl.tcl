c406e4b20ea15b15e3dac1015272f456a16437f9
Oleg Nemanov <lego12239@yandex.ru>
2020-10-09 14:48:16 +0300

    bump version to 1.0

--- htmltmpl.tcl
+++ htmltmpl.tcl
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package provide htmltmpl 0.10
+package provide htmltmpl 1.0
 
 namespace eval htmltmpl {
 variable tags [dict create]
